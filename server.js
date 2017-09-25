82e7fb6d62866fad77c3a7ced6320f5d302b669e
g1st <gintstan@gmail.com>
2017-09-25 19:23:15 +0100

    finished localhost functionallity

--- server.js
+++ server.js
@@ -22,15 +22,59 @@ app.get('/new/:url(*)', (req, res) => {
     if (validUrl.isUri(req.params.url)) {
       const cursor = db.collection('urls');
       const miniUrl = shortid.generate();
+      const query = {
+        original_url: {
+          $eq: req.params.url
+          }
+        };
 
-      cursor.insertOne({original_url: req.params.url, shortened_url: miniUrl})
-      res.send(`${req.params.url} is a valid uri and its miniUrl is ${miniUrl}`)
+      cursor.find(query).toArray((err, results) => {
+          if (err) throw err;
+
+          if (results.length > 0) {
+            // url already exists
+            cursor.update({original_url: req.params.url}, {$push: {shortened_url: miniUrl}});
+            res.send(`{"original_url": "${req.params.url}", "shortened_url": "localhost:${PORT}/${miniUrl}"}`);
+
+          } else {
+            // need add to db
+            cursor.insert({original_url: req.params.url, shortened_url: [miniUrl]});
+            res.send(`{"original_url": "${req.params.url}", "shortened_url": "localhost:${PORT}/${miniUrl}"}`);
+          }
+          db.close();
+
+        });
 
     } else {
-      res.send(`${req.params.url} not a URI`);
+      res.send(`{"error": "${req.params.url} Incorrect URL format."}`);
     }
-    db.close();
   });
 });
+
+app.get('/:url(*)', (req, res) => {
+  mongodb.connect(mongoUrl, (err, db) => {
+    if (err) throw err;
+    const query = {
+      shortened_url: {
+        $elemMatch: {
+          $eq: req.params.url
+        }
+      }
+    };
+    const cursor = db.collection('urls');
+    cursor.find(query).toArray((err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        res.redirect(results[0].original_url);
+      }
+      else {
+        res.send({error: "No corresponding shortlink found in the database."});
+      }
+      db.close();
+    });
+  }
+);
+});
+
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
